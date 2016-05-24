cab076b8bace9f02c2c16ec553b7ef0106abe35e
Raludza <rale.ilic1414@gmail.com>
2016-05-24 02:49:50 +0200

    module projects,form addProject

--- public/main/config/main.client.routes.js
+++ public/main/config/main.client.routes.js
@@ -8,10 +8,20 @@ angular.module('main').config(['$urlRouterProvider','$stateProvider',
         templateUrl: 'main/views/dashBoard.html',
         controller: 'myCtrl',
       })
+	  .state('dashBoard.projects', {
+        url: '/projects',
+        templateUrl: 'projects/views/listOfTasks.html',
+        controller: 'listOfProjectsCtrl'
+      })
       .state('dashBoard.tasks', {
         url: '/tasks',
         templateUrl: 'tasks/views/listOfTasks.html',
         controller: 'listOfTasksCtrl'
+      })
+	  .state('dashBoard.addProject', {
+        url: '/addProject',
+        templateUrl: 'projects/views/addProject.html',
+        controller: 'addProjectCtrl'
       })
        .state('dashBoard.addTask', {
         url: '/addTask',
