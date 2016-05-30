c2c26cedb95b93dd333d1b7e05f38348b509b68e
starcev <nemanja.s.993@hotmail.com>
2016-05-30 18:08:02 +0200

    Finished creating comment

--- app/controllers/tasks.server.controller.js
+++ app/controllers/tasks.server.controller.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose'),
 exports.create = function(req, res, next){
     var task = new Task(req.body);
     
-    //task.currentState.author = req.user._id;  // stavio sam author umesto creator
+    task.currentState.author = req.user._id;  // stavio sam author umesto creator
    
     var projectId = req.body.currentState.project._id;
     
